d941ef64ed874cd43d02d4b19323f8d169934668
Viterbo Rodriguez <Viterbo@users.noreply.github.com>
2025-07-20 14:09:51 -0300

    feat: initial RaceManager contract

--- contracts/Constants.sol
+++ contracts/Constants.sol
@@ -27,3 +27,26 @@ uint256 constant TOTAL_ACCELERATION          = MAX_ACCELERATION + MIN_ACCELERATI
 uint256 constant PRICE_PER_POINT             = 1 ether;
 uint256 constant BASE_RACE_COOLDOWN          = 1 days;
 uint256 constant BASE_FEED_COOLDOWN          = 1 days;
+
+// ---------------------- RaceManager ----------------------
+uint256 constant MAX_SEED_QUEUE_LENGTH                = 16;
+uint256 constant SEED_TIME_THRESHOLD                  = 5 minutes;
+uint256 constant SEED_HASH_THRESHOLD                  = 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+uint256 constant SEED_CHAIN_LENGTH                    = 4;
+
+uint256 constant MAX_FIXTURE_HORSES                   = 64;
+uint256 constant MAX_POINTS_DIFFERENCE_TOLERANCE      = 5;
+uint256 constant MAX_LEVELS_DIFFERENCE_TOLERANCE      = 2;
+uint256 constant FIXTURE_MIN_TIME_DISTANCE            = 1 hours;
+uint256 constant FIXTURE_MAX_TIME_DISTANCE            = 24 hours;
+uint256 constant FIXTURE_CONFIRM_TIME                 = 30 minutes;
+uint256 constant TIME_BETWEEN_RACES                   = 5 minutes;
+
+uint256 constant MIN_HORSES_PER_RACE                  = 2;
+uint256 constant MAX_HORSES_PER_RACE                  = 8;
+uint256 constant TOTAL_TRACK_LENGTH                   = 1000;
+uint256 constant MAX_HORSE_LEVEL_TRACK_MODIFIER       = 10;
+
+uint256 constant RACE_HORSE_INSCRIPTION_COST_PER_LEVEL = 1 ether;
+uint256 constant HORSE_NOT_CHOSEN_CONSOLATION_PRICE_MULTIPLIER = 1;
+uint256 constant RACE_TOTAL_PRICE_BASE                = 1 ether;
