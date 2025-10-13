c582e35066c43359cadf363b55b7e188d0e66a2f
Viterbo Rodriguez <Viterbo@users.noreply.github.com>
2025-10-13 02:28:11 -0300

    Refactor landing pages to use section button component

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,78 +1,15 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { SharedModule } from '@app/shared/shared.module';
-import { TabMenu } from 'primeng/tabmenu';
-import { MenuItem } from 'primeng/api';
-import { filter, Subscription } from 'rxjs';
+import { SectionButtonComponent } from '@app/components/section-button/section-button.component';
 
 @Component({
     standalone: true,
     selector: 'app-shop',
-    imports: [SharedModule, TabMenu, RouterOutlet],
-    template: `
-        <p-tabmenu
-            class="p-shop__tabs"
-            [model]="items"
-            [activeItem]="activeItem"
-            (activeItemChange)="onActiveItemChange($event)">
-        </p-tabmenu>
-
-        <div class="p-shop__tabs-content">
-            <router-outlet></router-outlet>
-        </div>
-    `,
+    imports: [SharedModule, RouterOutlet, NgIf, SectionButtonComponent],
+    templateUrl: './shop.component.html',
     styleUrls: ['./shop.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class ShopPage implements OnInit, OnDestroy {
-    items: MenuItem[] = [];
-    activeItem?: MenuItem;
-    private sub?: Subscription;
-
-    constructor(
-        private translate: TranslateService,
-        private router: Router
-    ) {}
-
-    ngOnInit(): void {
-        this.buildItems();
-        this.sub = this.router.events
-            .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
-            .subscribe(() => this.syncActiveFromUrl());
-        this.syncActiveFromUrl();
-        this.translate.onLangChange.subscribe(() => {
-            this.buildItems();
-            this.syncActiveFromUrl();
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.sub?.unsubscribe();
-    }
-
-    onActiveItemChange(item: MenuItem): void {
-        this.activeItem = item;
-    }
-
-    private buildItems(): void {
-        const shoesLabel = this.translate.instant('PAGES.SHOP.SHOES.NAME');
-        const accessoriesLabel = this.translate.instant('PAGES.SHOP.ACCESSORIES.NAME');
-        const jockeyLabel = this.translate.instant('PAGES.SHOP.JOCKEYS.NAME');
-
-        this.items = [
-            { label: shoesLabel, icon: 'pi pi-briefcase', routerLink: ['/shop', 'horseshoes'] },
-            { label: accessoriesLabel, icon: 'pi pi-star', routerLink: ['/shop', 'accessories'] },
-            { label: jockeyLabel, icon: 'pi pi-user', routerLink: ['/shop', 'jockeys'] }
-        ];
-    }
-
-    private syncActiveFromUrl(): void {
-        const segments = this.router.url.split('/');
-        const tab = segments[2] || 'horseshoes';
-        let index = 0;
-        if (tab === 'accessories') index = 1;
-        else if (tab === 'jockeys') index = 2;
-        this.activeItem = this.items[index];
-    }
-}
+export class ShopPage {}
